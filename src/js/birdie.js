ffdb8e7e859a0513b0baf0342648bb66c664d460
seda-karadeniz <sedayo98@gmail.com>
2021-05-20 22:45:09 +0200

    mouvement des ailes oiseau

--- src/js/birdie.js
+++ src/js/birdie.js
@@ -7,6 +7,10 @@ const birdie ={
         {sx:62,sy:982},
         {sx:118,sy:982}
     ],
+    maxAnimationStep: 2,
+    animationStep:0,
+    counterIntervall: 0,
+    maxIntervall: 5,
     width:34,
     height: 24,
     x:0,
@@ -29,12 +33,18 @@ const birdie ={
     },
 
     render(){
+        this.counterIntervall++;
+        if (!(this.counterIntervall % this.maxIntervall)){
+            this.counterIntervall = 0;
+            this.animationStep = this.animationStep < this.maxAnimationStep ? this.animationStep + 1 : 0; /*si animation step sera incrementer de un si animation step est plus petit que max animation step sinon remet 0*/
+
+        }
         this.game.context.save()
         this.game.context.translate(this.x,this.y);
         this.game.context.rotate(0);
         this.game.renderSpriteFrame({
-            sx: this.frames[0].sx,
-            sy:this.frames[0].sy,
+            sx: this.frames[this.animationStep].sx,
+            sy: this.frames[this.animationStep].sy,
             sw:this.width,
             sh:this.height,
             dx:-this.width/2,
